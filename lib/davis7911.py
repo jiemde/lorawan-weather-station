a6ff7bc2b9730ebc86a811bb5b8536dddeed8a0d
roadfox <fox@tcnet.ch>
2017-08-05 22:44:25 +0200

    set attn to max ATTN of 11dB to support 3,3V on ADC

--- lib/davis7911.py
+++ lib/davis7911.py
@@ -3,13 +3,13 @@ from machine import Pin, ADC
 
 class DAVIS7911(object):
   'Davis 7911 sensor library for Pycom LoPy'
-  
+
   # Constants
   ADC_MAX = 4000
   ADC_MIN = 150
   PIN_SPEED = 'P11'
   PIN_DIR = 'G3'
-  
+
   rotations = 0
   timeout = None
   adc = None
@@ -17,26 +17,28 @@ class DAVIS7911(object):
   # Pins
   pin_speed = None
   pin_dir = None
-  
+
   def __init__(self):
     self.timeout = time()
 
     try:
+      # Intialize the Speed IO
       self.pin_speed = Pin(self.PIN_SPEED, mode = Pin.IN, pull = Pin.PULL_UP)
       self.pin_speed.callback(Pin.IRQ_RISING, self.rotations_handler)
-      
+
+      # Initialize the Direction ADC
       self.adc = ADC()
-      self.pin_dir = self.adc.channel(pin = self.PIN_DIR)
+      self.pin_dir = self.adc.channel(pin = self.PIN_DIR, attn = ADC.ATTN_11DB)
     except Exception as e:
       # Should throw exception to stop execution
       pass
-    
+
   def rotations_handler(self, arg):
     self.rotations = self.rotations + 1
 
   def mph_to_ms(self, mph):
     return mph * 0.447
-    
+
   def dir_to_deg(self, dir):
     #pc2 = (dir - self.ADC_MIN) / ((self.ADC_MAX - self.ADC_MIN) / 360)
     pc = (dir - self.ADC_MIN) / (self.ADC_MAX - self.ADC_MIN)
@@ -68,12 +70,12 @@ class DAVIS7911(object):
       mph = self.rotations * (2.25 / delta_time)
     except Exception as e:
       mph = 0
-    
+
     self.rotations = 0
     self.timeout = time()
-    
+
     return round(self.mph_to_ms(mph), 1)
-    
+
   def get_dir(self):
     try:
       dir = self.pin_dir.value()

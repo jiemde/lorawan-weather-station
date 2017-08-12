29a12cce8a46dbb5056341aae2ca7a2d04398a00
roadfox <fox@tcnet.ch>
2017-08-12 20:10:12 +0200

    keys removed

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # Replace with your own OTAA keys
-dev_eui = '70B3D5499F671334' # Not yet sure if it's needed for OTAA on TheThingsNetwork, examples differ and i can't test with OTAA as i don't have a full gateway available
-app_eui = '70B3D57EF0006828'
-app_key = 'EC775FED31A122C9194F18374F8A2BBC'
+dev_eui = 'xxxxxxxxxxxxxxxx' # Not yet sure if it's needed for OTAA on TheThingsNetwork, examples differ and i can't test with OTAA as i don't have a full gateway available
+app_eui = 'xxxxxxxxxxxxxxxx'
+app_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
 # Replace with your own ABP keys
-dev_addr = '26011D1E'
-nwk_swkey = '65B75C139E779AC757EB1686616D7BA6'
-app_swkey = 'C166ECAEABF57E4BE5C7AAAB5454F554'
+dev_addr = 'xxxxxxx'
+nwk_swkey = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+app_swkey = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
